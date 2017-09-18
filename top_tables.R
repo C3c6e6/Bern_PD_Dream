7e52ced70673f1785a667f26f0b77797eae2203a
Cedric Simillion <cedric.simillion@dkf.unibe.ch>
2017-09-18 16:35:32 +0200

    Added the rangeGap statistic as well as a one-way ANOVA F-test.

--- top_tables.R
+++ top_tables.R
@@ -2,20 +2,41 @@ library(Biobase)
 library(parallel)
 options(mc.cores = 4)
 
+rangeGap <- function(values, valueFactor, q = c(0.05, 0.95)) {
+    factorMedians = tapply(values, valueFactor, median)
+    quantiles = tapply(values, valueFactor, quantile, q)
+    maxLevel = names(which.max(factorMedians))
+    minLevel = names(which.min(factorMedians))
+    distance = quantiles[[maxLevel]][1] - quantiles[[minLevel]][2]
+    globalQuantiles = quantile(values, q)
+    norm = globalQuantiles[2] - globalQuantiles[1]
+    gap = distance/norm
+    names(gap) = NULL
+    gap
+}
+
+oneWayTest <- function(values, variableFactor) {
+    testData = data.frame(v = values, f = variableFactor)
+    output = oneway.test(v ~ f, data = testData)
+    output
+}
+
 kruskalTopTable <- function(variable, eSet) {
     message(variable)
     eMatrix = exprs(eSet)
     variableFactor = factor(pData(eSet)[[variable]])
     topTable = fData(eSet)
     topTable$variable = variable
-    tests = apply(eMatrix, 1, kruskal.test, variableFactor)
+    tests = apply(eMatrix, 1, oneWayTest, variableFactor)
     topTable$p = sapply(tests, function(x) x$p.value)
     topTable$padj = p.adjust(topTable$p, method="fdr")
+    topTable$rangeGap = apply(eMatrix, 1, rangeGap, variableFactor)
     topTable = topTable[order(topTable$padj, topTable$p),]
     topTable
 }
 
 correlationTopTable <- function(variable, eSet) {
+    message(variable)
     eMatrix = exprs(eSet)
     variableValues = pData(eSet)[[variable]] + runif(ncol(eSet), max=1e-6)
     topTable = fData(eSet)
